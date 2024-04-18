c9be98528d689e70c7547eab66274d1df2258b2e
mkhop <mkh.victor@gmail.com>
2024-04-18 23:44:45 +0630

    final version of intended purpose, need to refurbish

--- fileSorter.py
+++ fileSorter.py
@@ -1,16 +1,16 @@
-import os, shutil
+import os, shutil, re
 
 path = r"D:\ALVLS\megaFolder"
 
 
-# Removed the IAL_ at the start
-for file in os.listdir(path):
-    file_name = file.split(".")[0]
-    if "IAL_" in file:
-        newFileName = file_name.replace("IAL_", "")
-        shutil.move(f"{path}\{file}", f"{path}\{newFileName}.pdf")
+# # Removed the IAL_ at the start
+# for file in os.listdir(path):
+#     file_name = file.split(".")[0]
+#     if "IAL_" in file:
+#         newFileName = file_name.replace("IAL_", "")
+#         shutil.move(f"{path}\{file}", f"{path}\{newFileName}.pdf")
 
-# Initializing the variables
+# # Initializing the variable
 decisionOne = "WDM11"
 pureOne = "WMA11"
 pureTwo = "WMA12"
@@ -43,6 +43,34 @@ assigningFolder()
 
 # Adding new features
 
-def newFeature():
-    print()
-    print("new")
+def monthYear():
+    dateFormat = "^[0-9]{2}[0-9]{2}$"
+    paperMonth = paperYear = itemsList = []
+
+    for paperType in paperCodes:
+        print(f"Now in {path}\{paperType} PDFs")
+
+        for file in os.listdir(f"{path}\{paperType} PDFs"):
+            paperDate = file.split("_")[2]
+
+            if re.search(dateFormat, paperDate):
+                paperMonth = re.findall(dateFormat, paperDate)[0]
+
+                if os.path.exists(f"{path}\{paperType} PDFs\{paperMonth}"):
+                    shutil.move(f"{path}\{paperType} PDFs\{file}", f"{path}\{paperType} PDFs\{paperMonth}")
+                    print("Paper Moved")
+                else:
+                    os.mkdir(f"{path}\{paperType} PDFs\{paperMonth}")
+                    shutil.move(f"{path}\{paperType} PDFs\{file}", f"{path}\{paperType} PDFs\{paperMonth}")
+                    print("Paper Moved")
+            else:
+                continue
+
+def otherPapers():
+    for file in os.listdir(path):
+        if file.endswith(".pdf"):
+            os.remove(f"{path}\{file}")
+
+otherPapers()
+# monthYear()
+
