896f25319e18c3c071d7738dd25dde8db9b717ab
mkhop <mkh.victor@gmail.com>
2024-04-18 16:23:55 +0630

    complete

--- fileSorter.py
+++ fileSorter.py
@@ -11,16 +11,13 @@ for file in os.listdir(path):
         shutil.move(f"{path}\{file}", f"{path}\{newFileName}.pdf")
 
 # Initializing the variables
-decisionOne = "WDM11_01"
-pureOne = "WMA11_01"
-pureTwo = "WMA12_01"
-statsOne = "WST11_01"
+decisionOne = "WDM11"
+pureOne = "WMA11"
+pureTwo = "WMA12"
+statsOne = "WST01"
 
 paperCodes = [decisionOne, pureOne, pureTwo, statsOne]
-
-
 # Creating folders
-
 def createFolder():
     for paperCode in paperCodes:
         if os.path.exists(f"{path}\{paperCode}"):
@@ -28,3 +25,18 @@ def createFolder():
         else:
             os.mkdir(f"{path}\{paperCode}")
             print(f"{paperCode} folder created")
+
+createFolder()
+
+def assigningFolder():
+    for file in os.listdir(path):
+        fileCode = file.split("_")[0]
+
+        if fileCode in paperCodes:
+            shutil.move(f"{path}\{file}", f"{path}\{fileCode} PDFs")
+            print(f"{file} moved to {fileCode} folder")
+        else:
+            print(f"{file} is not in the paperCodes list")
+        
+
+assigningFolder()
