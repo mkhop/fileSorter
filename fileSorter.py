4e0e843c14569b582cc3f07d624ed383568848ea
mkhop <mkh.victor@gmail.com>
2024-04-18 15:58:19 +0630

    createFolder process

--- fileSorter.py
+++ fileSorter.py
@@ -3,11 +3,28 @@ import os, shutil
 path = r"D:\ALVLS\megaFolder"
 
 
-
+# Removed the IAL_ at the start
 for file in os.listdir(path):
     file_name = file.split(".")[0]
     if "IAL_" in file:
         newFileName = file_name.replace("IAL_", "")
         shutil.move(f"{path}\{file}", f"{path}\{newFileName}.pdf")
 
-print("success")
+# Initializing the variables
+decisionOne = "WDM11_01"
+pureOne = "WMA11_01"
+pureTwo = "WMA12_01"
+statsOne = "WST11_01"
+
+paperCodes = [decisionOne, pureOne, pureTwo, statsOne]
+
+
+# Creating folders
+
+def createFolder():
+    for paperCode in paperCodes:
+        if os.path.exists(f"{path}\{paperCode}"):
+            print(f"{paperCode} folder already exists")
+        else:
+            os.mkdir(f"{path}\{paperCode}")
+            print(f"{paperCode} folder created")
