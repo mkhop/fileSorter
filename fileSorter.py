bd1fb9a3abfaa030c7446850b012af87097e052d
mkhop <mkh.victor@gmail.com>
2024-04-20 11:00:40 +0630

    creates custom folders

--- fileSorter.py
+++ fileSorter.py
@@ -7,15 +7,32 @@ def checkFileCount():
     name = input("File Type: ")
     count = 0
 
+    # Running file until user input is null
     while name.strip() != "":
         for file in os.listdir(path):
+
+            # Checking if file contains user input
             if name in file:
                 count += 1
-        
-        respectiveFileCount.update({name: count})
 
+
+                # If the folder already exists, moving file to the folder
+                if os.path.exists(f"{path}/{name}"):
+                    shutil.move(f"{path}/{file}", f"{path}/{name}")
+                
+                # Creating and moving file to newly created folder
+                else:
+                    os.mkdir(f"{path}/{name}")
+                    shutil.move(f"{path}/{file}", f"{path}/{name}")
+
+        respectiveFileCount.update({name: count})
+        print(f"Created a {name} folder with {count} files")
         name = input("File Type: ")
         count = 0
+
+         
+        
+
     return respectiveFileCount
 
     
